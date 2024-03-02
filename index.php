0ea0c3b4488c0e0543d5ef40cc681d370989fb23
danieledisco <daniele.disco.1965@gmail.com>
2024-03-02 16:27:47 +0100

    Snack 1

--- index.php
+++ index.php
@@ -1,5 +1,61 @@
 <?php
 
+$tappa = [
+    [
+        'home' => 'Pinerolo',
+        'guest' => 'Mondovì',
+        'rate_home' => 60,
+        'rate_guest' => 55
+    ],
+    [
+        'home' => 'Fossano',
+        'guest' => 'Cavalermaggiorte',
+        'rate_home' => 40,
+        'rate_guest' => 35
+    ],
+    [
+        'home' => 'Chivasso',
+        'guest' => 'Sangro',
+        'rate_home' => 33,
+        'rate_guest' => 88
+    ],
+    [
+        'home' => 'Susa',
+        'guest' => 'Giaveno',
+        'rate_home' => 30,
+        'rate_guest' => 90
+    ],
+    [
+        'home' => 'Ala di stura',
+        'guest' => 'Barriera di milano',
+        'rate_home' => 12,
+        'rate_guest' => 120
+    ],
+    [
+        'home' => 'Cit Turin',
+        'guest' => 'Quadrilatero Romano',
+        'rate_home' => 160,
+        'rate_guest' => 58
+    ]
+
+];
+
+// Test puro php
+//
+// for ($i = 0; $i < count($tappa); $i++) {
+//    echo "<pre>", $tappa[$i]['home'] . ' - ' . $tappa[$i]['guest'] . ' | ' . $tappa[$i]['rate_home'] . '-' . $tappa[$i]['rate_guest'], "</pre>";
+// }
+
+
+
+
+
+
+
+
+
+
+
 
 ?>
 
@@ -14,7 +70,18 @@
 </head>
 
 <body>
+    <h1>Snack 1</h1>
+    <?php
+    for ($i = 0; $i < count($tappa); $i++) {
+    ?>
+        <span><?php echo $tappa[$i]['home'] . ' - ' ?></span>
+        <span><?php echo $tappa[$i]['guest'] . ' | ' ?></span>
+        <span><?php echo $tappa[$i]['rate_home'] . '-' ?></span><span><?php echo $tappa[$i]['rate_guest'] ?></span>
+        <p>
 
+        </p>
+    <?php } ?>
+    <h1>Fine Snack 1</h1>
 </body>
 
 </html>
